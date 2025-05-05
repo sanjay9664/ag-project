bb6b149c8715b52e3dccf4e09e1dc198e999df8d
Sanjay Gupta <guptaji30749@gmail.com>
2025-05-05 17:48:13 +0530

    resolve

--- resources/views/backend/pages/sites/superadmin-site-details.blade.php
+++ resources/views/backend/pages/sites/superadmin-site-details.blade.php
@@ -177,11 +177,9 @@
                         ?>
 
                         <?php
-    $numericValue = floatval($addValue); // Convert string to float
-    $hours = floor($numericValue);
-    $minutes = round(($numericValue - $hours) * 60);
-?>
-
+                            $hours = floor($addValue);
+                            $minutes = round(($addValue - $hours) * 60);
+                        ?>
                         <div class="card mb-1 shadow-sm border-0">
                             <div class="card-body text-center">
                                 <div class="d-flex align-items-center gap-4 mb-1">
@@ -1559,12 +1557,10 @@
                     }
                     ?>
 
-<?php
-    $numericValue = floatval($addValue); // Convert string to float
-    $hours = floor($numericValue);
-    $minutes = round(($numericValue - $hours) * 60);
-?>
-
+                        <?php
+                            $hours = floor($addValue);
+                            $minutes = round(($addValue - $hours) * 60);
+                        ?>
 
                         <div class="card mb-1 shadow-sm border-0">
                             <div class="card-body text-center">
